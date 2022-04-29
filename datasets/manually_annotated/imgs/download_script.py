bd89ee86984b02a02a40049e5003806aed3cdcd9
Carlos X. Soto <csoto@bnl.gov>
2022-04-29 15:33:25 -0400

    datasets

---
+++ datasets/manually_annotated/imgs/download_script.py
@@ -0,0 +1,3 @@
+#! /usr/bin/env python
+
+# download, render, and crop figures from PMCOA (no direct file hosting due to copyright)
---
+++ datasets/zhou_2021/README.md
@@ -0,0 +1,1 @@
+From [https://github.com/csuvis/BarchartReverseEngineering](https://github.com/csuvis/BarchartReverseEngineering).
